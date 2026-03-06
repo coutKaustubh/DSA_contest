63137f11045252e1d29acf16f6f4b4f95694557f
KAUSTUBH SRIVASTAVA <kaustubhsrivastavajee22@gmail.com>
2026-03-06 09:29:03 +0530

    leetcode1784

--- Leetcode/1784.cpp
+++ Leetcode/1784.cpp
@@ -1,7 +1,22 @@
+// class Solution {
+// public:
+//     bool checkOnesSegment(string s) {
+//         int c=0;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i] == '1')c++;
+//             else c=0;
+//             if(c==2)return true;
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool checkOnesSegment(string s) {
         int c=0;
+        if(s.size() == 1 && s[0] == '1')return true;
         for(int i=0;i<s.size();i++){
             if(s[i] == '1')c++;
             else c=0;
