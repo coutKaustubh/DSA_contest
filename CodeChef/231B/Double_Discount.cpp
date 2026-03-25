b22b6444fefe281af618bb361cf51a74f57ea710
KAUSTUBH SRIVASTAVA <kaustubhsrivastavajee22@gmail.com>
2026-03-26 00:22:21 +0530

    ques1 done

---
+++ CodeChef/231B/Double_Discount.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+
+
+using namespace std;
+
+
+#define ll long long
+#define f(i,n) for (ll i = 0; i < n; i++)
+#define ia(a,n) \
+    ll a[n];     \
+    f(i,n) cin >> a[i]
+#define iv(v, n)     \
+    vector<ll> v(n); \
+    f(i,n) cin >> v[i]
+
+#define create_matrix(mat, n, m) vector<vector<ll>> mat(n, vector<ll>(m));
+#define input_matrix(mat, n, m) f(i,n) f(j,m) cin >> mat[i][j];
+
+    
+#define MOD (1000000007)
+#define INF 1000000000000000000LL // Infinity for ll
+#define mp make_pair
+#define nline '\n'
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+
+void solve(){
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> a(n), b(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cin >> b[i];
+
+    ll mx = 0;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+
+            ll big = max(a[i], a[j]);
+            ll small = min(a[i], a[j]);
+
+            ll discount = min(big / 2, 100LL);
+            ll cost = small + (big - discount);
+
+            if(cost <= k){
+                mx = max(mx, b[i] + b[j]);
+            }
+        }
+    }
+
+    cout << mx << '\n';
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
