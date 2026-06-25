feea51e546c05169025f316c67ecb109d0a541a1
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-06-25 13:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (61.34%) - LeetSync

--- Leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ Leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,42 +3,30 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        int n = n1+n2;
-        int idx1 = n/2;
-        int idx2 = n/2 - 1;
-        int ele1,ele2;
-        int c=0;
-        int i=0,j=0;
-        while(i < n1 && j < n2){
-            if(nums1[i] < nums2[j]){
-                if(c == idx1) ele1 = nums1[i];
-                if(c == idx2) ele2 = nums1[i];
-                c++;
-                i++;
+        if(n1>n2)return findMedianSortedArrays(nums2,nums1); //assuming 1st is small
+        int n = n1+n2;  
+        int left = (n+1)/2;
+        int low = 0;
+        int high = n1;
+
+        while(low<=high){
+            int mid1 = (low+high) >> 1;
+            int mid2 =  left-mid1;
+            int l1 = INT_MIN,l2=INT_MIN;
+            int r1 = INT_MAX,r2=INT_MAX;
+            if(mid1 < n1)r1 = nums1[mid1];
+            if(mid2<n2)r2 = nums2[mid2];
+            if(mid1-1>=0 )l1 =nums1[mid1-1];
+            if(mid2-1>=0 )l2 =nums2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                if(n&1)return max(l1,l2);
+                else return ((double)max(l1,l2) + min(r1,r2)) / 2.0;
             }
-            else{
-                if(c == idx1) ele1 = nums2[j];
-                if(c == idx2) ele2 = nums2[j];
-                c++;
-                j++;
-            }
-        }
+            else if(l1>r2)high = mid1-1;
+            else low = mid1+1;
+            
 
-        while(i < n1){
-            if(c == idx1) ele1 = nums1[i];
-            if(c == idx2) ele2 = nums1[i];
-            c++;
-            i++;
         }
-
-        while(j < n2){
-            if(c == idx1) ele1 = nums2[j];
-            if(c == idx2) ele2 = nums2[j];
-            c++;
-            j++;
-        }
-
-        if(n&1)return (double)ele1;
-        else return (double)(ele1+ele2)/2;
+        return 0;
     }
 };
