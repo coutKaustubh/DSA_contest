fd621fac039e6fea6d098491cf50ce5ad7f713b1
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-07-03 13:21:28 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (31.14%) - LeetSync

--- Leetcode/169-majority-element/majority-element.cpp
+++ Leetcode/169-majority-element/majority-element.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto x:nums)mp[x]++;
-        int ele = -1,mx=-1;
-        for(auto x:mp){
-            if(x.second > mx){
-                mx = x.second;
-                ele = x.first;
-            }
-        }
-        return ele;
+        int ele =-1;
+        int cnt = 0;
+        // intuition - cnt ++ when we find similar ele else cnt -- , so if at end cnt>0 so that ele is majority
 
+        for(int i=0;i<nums.size();i++){
+            if(cnt==0)ele = nums[i];
+            if(ele == nums[i])cnt++;
+            else cnt--;
+        }
+
+        return ele;
     }
 };
