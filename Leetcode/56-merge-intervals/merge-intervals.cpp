424f1b7c3318b4a68df3c6a9d790c6f6d8873c29
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-07-31 16:19:50 +0530

    Time: 4 ms (66.03%) | Memory: 23.8 MB (59.50%) - LeetSync

--- Leetcode/56-merge-intervals/merge-intervals.cpp
+++ Leetcode/56-merge-intervals/merge-intervals.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& it) {
+        if(it.size()==1)return it;
         vector<vector<int>>ans;
         sort(it.begin(), it.end());
-        for(int i=0;i<it.size();i++){
-            int start = it[i][0];
-            int end = it[i][1];
-            if(!ans.empty() && end<=ans.back()[1]){
-                // ans = [[1,6] , [8,11]]
-                // it = [..... [9,10]]
-                // so end = 10, start = 9 , and we check end<= ans ke last element ka dusra index i.e 11
-                // so if 10<=11 (true) therefore already inside
-                continue;
+        int start = it[0][0];
+        int end = it[0][1];
+        int i=1;
+        while(i<it.size()){
+            if(it[i][0] <= end){
+                end = max(end,it[i][1]);
             }
-           
-                for(int j=i+1;j<it.size();j++){
-                    if(it[j][0] <= end){
-                        end = max(end , it[j][1]);
-                    }
-                    else break; // bcz ek bar nhi aaya andar to aage nhi ayega because we sorted whole matrix in start
-                }
-            ans.push_back({start,end});
+            else{
+                ans.push_back({start,end});
+                start = it[i][0];
+                end = it[i][1];
+            }
+            i++;
         }
+        ans.push_back({start, end});
         return ans;
     }
 };
