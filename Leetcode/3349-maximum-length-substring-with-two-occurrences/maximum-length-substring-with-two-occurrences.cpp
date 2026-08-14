af025a186390c9adc8771a23252ded11cda02877
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-08-14 15:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (57.72%) - LeetSync

--- Leetcode/3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ Leetcode/3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maximumLengthSubstring(string s) {
         unordered_map<char, int> freq;
-        for(char c = 'a'; c <= 'z'; c++)freq[c] = 0;
 
         int l =0,r=0,mx=0;
         while(r<s.size()){
