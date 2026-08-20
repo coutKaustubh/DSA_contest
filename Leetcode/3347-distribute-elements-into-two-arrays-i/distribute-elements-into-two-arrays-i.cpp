e1ccd75d93fe723af4a23b3a49e1c56730bad15b
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-08-20 22:56:42 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (83.63%) - LeetSync

--- Leetcode/3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ Leetcode/3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
-        vector<int> A[2]={{nums[0]}, {nums[1]}};
-        const int n=nums.size();
-        for(int i=2; i<n; i++){
-            A[A[0].back()<=A[1].back()].push_back(nums[i]);
+        vector<int> arr1 = {nums[0]};
+        vector<int> arr2 = {nums[1]};
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (arr1.back() > arr2.back())
+                arr1.push_back(nums[i]);
+            else
+                arr2.push_back(nums[i]);
         }
-        A[0].insert(A[0].end(), A[1].begin(), A[1].end());
-        return A[0];
+
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        return arr1;
     }
 };
-
