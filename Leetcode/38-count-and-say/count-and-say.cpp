e9f1b843f90efd265188012ef952b6bd482e2219
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-08-21 23:51:08 +0530

    Time: 3 ms (81.13%) | Memory: 10 MB (33.67%) - LeetSync

--- Leetcode/38-count-and-say/count-and-say.cpp
+++ Leetcode/38-count-and-say/count-and-say.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void helperMap(string &s){
-        vector<pair<char,int>> freq;
+    void helperMap(string &s) {
+        string temp;
         for(int i = 0; i < s.size(); ) {
             char ch = s[i];
             int cnt = 0;
+
             while(i < s.size() && s[i] == ch) {
                 cnt++;
                 i++;
             }
 
-            freq.push_back({ch, cnt});
-        }
-        s="";
-        for(auto [x,y]:freq){
-            s += (to_string(y));
-            s.push_back(x);
+            temp += to_string(cnt);
+            temp.push_back(ch);
         }
 
+        s = temp;
     }
+
     string countAndSay(int n) {
-        if(n==1)return "1";
         string ans = "1";
-        for(int i=2;i<=n;i++){
+        for(int i = 2; i <= n; i++) {
             helperMap(ans);
         }
         return ans;
