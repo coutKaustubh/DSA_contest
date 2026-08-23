2f31ca20e5cfe438168d9f87965ee10e93b9b3f7
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-08-24 00:53:34 +0530

    Time: 5 ms (78.08%) | Memory: 23.3 MB (81.27%) - LeetSync

---
+++ Leetcode/1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int pref=0,suff=1;
+        int n = s.size();
+        if (n == 0)
+            return "";
+        vector<int>LPC(n,0); //longest prefix-suffix combination by KMP method
+        LPC[0] = 0;
+        int k=1;
+        while(suff<n){
+            if(s[pref] == s[suff]){
+                LPC[suff] = LPC[suff-1]+1;
+                suff++;
+                pref++;
+            }
+            else{
+                int actualsuff=suff;
+                int c=0;
+                while(pref>=0){
+                    if(s[pref] != s[suff]){
+                        pref--;
+                        c=0;
+                        suff = actualsuff;
+                    }
+                    else{
+                        pref--;
+                        suff--;
+                        c++;
+                    }
+                }
+                pref = c;
+                LPC[actualsuff] = c;
+                suff = actualsuff+1;
+            }
+        } 
+        int size = LPC[n-1];
+
+        return s.substr(0,size);
+    } 
+};
