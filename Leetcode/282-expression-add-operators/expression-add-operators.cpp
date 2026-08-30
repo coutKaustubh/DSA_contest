4f39461146539723d95d461644223444f42a419a
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-08-30 18:40:31 +0530

    Time: 463 ms (8.95%) | Memory: 93.5 MB (25.51%) - LeetSync

--- Leetcode/282-expression-add-operators/expression-add-operators.cpp
+++ Leetcode/282-expression-add-operators/expression-add-operators.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    void solve(int curIdx,string sumPath,long sum,long prev, vector<string>&result,string s,int target){
-        if(curIdx == s.size() && sum == target){
-            result.push_back(sumPath);
+    void solve(int curIdx,string finalPath,long final,long prev, vector<string>&ans,string s,int target){
+        if(curIdx == s.size() && final == target){
+            ans.push_back(finalPath);
             return;
         }
-        for(int j = curIdx;j<s.size();j++){
-            if (j>curIdx && s[curIdx] == '0')break;
 
+        for(int j = curIdx;j<s.size();j++){
+            if (j>curIdx && s[curIdx] == '0')break; //leading 0 uda do
             long num = stol(s.substr(curIdx,j-curIdx+1));
             string tempPath = s.substr(curIdx,j-curIdx+1);
-
-            if(curIdx==0)solve(j+1 , tempPath,num,num,result,s,target);
+            if(curIdx == 0)solve(j+1 ,tempPath,num,num,ans,s,target);
             else{
-                solve(j+1,sumPath+'+'+tempPath,sum+num,num,result,s,target);
-                solve(j+1,sumPath+'-'+tempPath,sum-num,-num,result,s,target);
-                solve(j+1,sumPath+'*'+tempPath,sum-prev+prev*num,prev*num,result,s,target);
+                //+
+                solve(j+1 ,finalPath+"+"+tempPath,num+final,num,ans,s,target);
+                solve(j+1 ,finalPath+"-"+tempPath,final-num,-num,ans,s,target);
+                solve(j+1 ,finalPath+"*"+tempPath, final-prev+ prev*num, prev*num,ans,s,target);
             }
         }
+
     }
     vector<string> addOperators(string num, int target) {
-        vector<string>result;
-        solve(0,"",0,0,result,num,target);
-        return result;
+        vector<string>ans;
+        solve(0,"",0,0,ans,num,target);
+        return ans;
     }
-};
+};  
