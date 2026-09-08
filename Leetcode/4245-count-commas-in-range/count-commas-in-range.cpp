19bc964754b5506887a39f50d558c0421a715e32
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-09-08 15:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

--- Leetcode/4245-count-commas-in-range/count-commas-in-range.cpp
+++ Leetcode/4245-count-commas-in-range/count-commas-in-range.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     int countCommas(int n) {
 
-        if (n <= 999)
-            return 0;
+        if (n <= 999)return 0;
 
-        if (n <= 999999)
-            return n - 999;
-
-        else {
-            return 999000 + 2 * (n - 999999);
-        }
+        return n-999;
     }
 };
+
