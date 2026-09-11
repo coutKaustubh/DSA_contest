0ca8472275e5ac96f9927f1ac5df8a219c1d7839
Kaustubh Srivastava <kaustubhsrivastavajee22@gmail.com>
2026-09-12 01:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (76.91%) - LeetSync

---
+++ Leetcode/393-utf-8-validation/utf-8-validation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int still_left = 0;
+        for(int i = 0; i <data.size(); i++){
+            int x = data[i];
+            if(!still_left){
+                if((x >> 5) == 0b110) still_left = 1;    
+                else if((x >> 4) == 0b1110) still_left = 2;                       
+                else if((x >> 3) == 0b11110) still_left = 3;                      
+                else if((x >> 7) != 0) return false;       
+            } 
+            else {
+                if((x >> 6) != 0b10) return false;
+                still_left--;
+            }
+        }
+        return (still_left == 0);           
+    }                                       
+};
